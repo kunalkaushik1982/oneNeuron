d16611115f8656040beffde94e94e7856af9663f
Kunal Kaushik <kunkaush1@publicisgroupe.net>
2021-09-19 21:16:06 +0530

    testing doc string extension

--- utils/all_utils.py
+++ utils/all_utils.py
@@ -10,11 +10,13 @@ plt.style.use("fivethirtyeight")
 
 def prepare_data(df):
     """it is used to separate the dependent variables and independent features
+
     Args:
         df (pd.DataFrame): its the pandas DataFrame to
+
     Returns:
         tuple: it returns the tuples of dependent variables and independent variables
-    """
+    """ 
     logging.info("Preparing the data by segregating the independent and dependent variables")
     x = df.drop("y", axis=1)
     y = df["y"]
